fb4c5d9fc81a07669a3d978a60536272822acac5
Csaba Vadász <csabavadasz79@gmail.com>
2024-05-21 11:56:10 +0200

    explanation and usage of call by name

--- src/main/scala/lecture/part01basics/CBNvsCBV.scala
+++ src/main/scala/lecture/part01basics/CBNvsCBV.scala
@@ -32,4 +32,24 @@ object CBNvsCBV extends App {
   private def printFirst(numByValue: Int, numByName: => Int) = println(numByValue)
   // printFirst(infinite(), 34) // StackOverflowError!
   printFirst(34, infinite()) // output: 34 infinite() will never evaluate! that reason WE DO NOT GET StackOverflowError
+
+  // Call by name is used:
+  /*
+  1. Lazy Evaluation
+  Call by name is beneficial when you want to delay the evaluation of an argument until it is actually used within the
+  function. This is particularly useful for optimizing performance when the argument is computationally expensive to
+  evaluate and might not always be needed.
+  2. Control Structures
+  Call by name is useful for implementing custom control structures that require lazy evaluation, such as loops,
+  retries, or conditionals.
+  3. Resource Management
+  Call by name can be useful in resource management patterns, such as ensuring that resources are properly closed after use.
+  4. Default Arguments
+  Call by name can be used to provide default arguments that are computed lazily.
+
+  Conclusion
+  Call by name is a powerful feature in Scala that enables lazy evaluation, custom control structures, efficient
+  resource management, and lazy default arguments. Use it when you need to delay evaluation of an argument until it is
+  actually needed, which can lead to more efficient and expressive code.
+  */
 }
